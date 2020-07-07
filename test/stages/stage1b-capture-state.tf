21a744cbdc6f746f6f5c7cfad94bca34645f15cc
Sean Sundberg <seansund@us.ibm.com>
2020-07-06 21:54:21 -0500

    Updates OLM to v1.15.1 (#1)
    * Updates OLM to v1.15.1
    * Adds GitHub workflows

---
+++ test/stages/stage1b-capture-state.tf
@@ -0,0 +1,17 @@
+module "dev_capture_olm_state" {
+  source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
+
+  cluster_type             = module.dev_cluster.type_code
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  namespace                = var.cluster_type == "ocp4" ? "openshift-operator-lifecycle-manager" : "olm"
+  output_path              = "${path.cwd}/cluster-state/before"
+}
+
+module "dev_capture_operator_state" {
+  source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
+
+  cluster_type             = module.dev_cluster.type_code
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  namespace                = var.cluster_type == "ocp4" ? "openshift-operators" : "operators"
+  output_path              = "${path.cwd}/cluster-state/before"
+}
