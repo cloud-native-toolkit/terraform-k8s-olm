d394adb9b6b36970d7c3dc88379952677973a22b
seansund <seansund@users.noreply.github.com>
2020-07-15 05:02:44 +0000

    deploy: 3891eb07793e975e839f0123e9c5ffad632c3ee9

--- test/stages/stage2-olm.tf
+++
@@ -1,7 +0,0 @@
-module "dev_software_olm" {
-  source = "./module"
-
-  cluster_config_file      = module.dev_cluster.config_file_path
-  cluster_version          = ""
-  cluster_type             = module.dev_capture_olm_state.cluster_type == "" ? module.dev_capture_olm_state.cluster_type : module.dev_capture_operator_state.cluster_type
-}
