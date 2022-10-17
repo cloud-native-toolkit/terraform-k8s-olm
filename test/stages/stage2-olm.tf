a8d11f2052fd5b8c9801432de29a3047aae8ff58
Sean Sundberg <seansund@us.ibm.com>
2022-10-17 12:35:30 -0500

    Cleans up test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-olm.tf
+++ test/stages/stage2-olm.tf
@@ -3,5 +3,5 @@ module "dev_software_olm" {
 
   cluster_config_file      = module.dev_cluster.config_file_path
   cluster_version          = ""
-  cluster_type             = module.dev_capture_olm_state.cluster_type == "" ? module.dev_capture_olm_state.cluster_type : module.dev_capture_operator_state.cluster_type
+  cluster_type             = module.dev_cluster.cluster_type
 }
